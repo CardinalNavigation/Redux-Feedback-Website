2acc7fa2993eb58eea258cd1dc9ac2a9afddfd3d
Matthew Johnson <mtthw.jhnson@gmail.com>
2023-09-22 21:45:25 -0500

    added redux logger, react-redux and got the website to load again after adding boilerplate code

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
-import { Provider } from 'react';
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 
 const feedbackData = (state = [], action) => {
     if (action.type === 'FEEDBACK_PASS_ALONG') {
@@ -24,7 +24,7 @@ const reduxStore = createStore(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Provider>
+        <Provider store = {reduxStore}>
             <App />
         </Provider>
     </React.StrictMode>
